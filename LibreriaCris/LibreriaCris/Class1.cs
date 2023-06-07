e600a44583ab0e8b0b46125065e60e14a999fef3
cristianedgargutierrez <criguva@gmail.com>
2023-06-07 05:53:05 -0400

    primer commit

---
+++ LibreriaCris/LibreriaCris/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace LibreriaCris
+{
+    public class Class1
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void Log2(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void Daniel(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ PruebaDependencia/PruebaDependencia/Class9.cs
@@ -0,0 +1,11 @@
+﻿namespace PruebaDependencia
+{
+    public class Class9
+    {
+        public void hola()
+        {
+            LibreriaCris.Class1 d = new LibreriaCris.Class1();
+            d.;
+        }
+    }
+}
